7cbe805e7200fe7ddba555ab10144ba4ca5c9a3b
Ethan Rogers <erogers6264@gmail.com>
2016-07-08 14:28:04 -0700

    Change German special characters to their ascii counterparts

--- wordbank.py
+++ wordbank.py
@@ -2,22 +2,20 @@
 # Module should return a list, words, from which a random selection is chosen for
 # use in a game of hangman. Will possibly expand to retrieve a list of words of a
 # user chosen category. Or something dynamic of that nature.
-# TODO Think about umlauts. Will that have an effect on how the input is given?
-# Replace umlauts with ae ue oe?
 #
 
-words = ["sein", "haben", "werden", "können", "müssen", "sagen", "machen",
+words = ["sein", "haben", "werden", "koennen", "muessen", "sagen", "machen",
 "geben", "kommen", "sollen", "wollen", "gehen", "wissen", "sehen", "lassen",
-"stehen", "finden", "bleiben", "liegen", "heißen", "denken", "nehmen", "tun",
-"dürfen", "glauben", "halten", "nennen", "mögen", "zeigen", "führen",
-"sprechen", "bringen", "leben", "fahren", "meinen", "fragen", "kennen",
+"stehen", "finden", "bleiben", "liegen", "heissen", "denken", "nehmen", "tun",
+"duerfen", "glauben", "halten", "nennen", "moegen", "zeigen", "fuehren",
+"sprechen,", "bringen", "leben", "fahren", "meinen", "fragen", "kennen",
 "gelten", "stellen", "spielen", "arbeiten", "brauchen", "folgen", "lernen",
-"bestehen", "verstehen", "setzen", "bekommen", "beginnen", "erzählen",
-"versuchen", "schreiben", "laufen", "erklären", "entsprechen", "sitzen",
-"ziehen", "scheinen", "fallen", "gehören", "entstehen", "erhalten", "treffen",
+"bestehen", "verstehen", "setzen", "bekommen", "beginnen", "erzaehlen",
+"versuchen", "schreiben", "laufen", "erklaeren", "entsprechen", "sitzen",
+"ziehen", "scheinen", "fallen", "gehoeren", "entstehen", "erhalten", "treffen",
 "suchen", "legen", "vorstellen", "handeln", "erreichen", "tragen", "schaffen",
 "lesen", "verlieren", "darstellen", "erkennen", "entwickeln", "reden",
 "aussehen", "erscheinen", "bilden", "anfangen", "erwarten", "wohnen",
-"betreffen", "warten", "vergehen", "helfen", "gewinnen", "schließen", "fühlen",
+"betreffen", "warten", "vergehen", "helfen", "gewinnen", "schliessen", "fuehlen",
 "bieten", "interessierento", "erinnern", "ergeben", "anbieten", "studieren",
 "verbinden", "ansehen", "fehlen", "bedeuten", "vergleichen"]

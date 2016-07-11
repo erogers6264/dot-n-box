f044e8ba17e5467d98e68b56a2776cedc6a745b5
Ethan Rogers <erogers6264@gmail.com>
2016-07-11 11:10:29 -0700

    Add a function to get a guess from user

--- hangman.py
+++ hangman.py
@@ -5,10 +5,17 @@ target = random.choice(words)
 attempts = input("How many attempts to guess would you like? ")
 attempts_remaining = attempts
 
-for x in xrange(1, attempts+1):
+
+def getGuess():
+	"""Request that the user enter a character
+	TODO Validate the user input"""
 	guess = raw_input("Please enter a single character: ")
+	return guess
+
+
+for x in xrange(1, attempts+1):
 	for i, c in enumerate(target):
-		if guess == c:
+		if getGuess() == c:
 			print True
 		else:
 			attempts -= 1

6ed2c9a61cd30a3b3867c84690bcc1c8a5f24f86
Ethan Rogers <erogers6264@gmail.com>
2016-07-11 09:52:15 -0700

    Check to see if guess is in target

--- hangman.py
+++ hangman.py
@@ -6,8 +6,10 @@ attempts = input("How many attempts to guess would you like? ")
 attempts_remaining = attempts
 
 for x in xrange(1, attempts+1):
-	guess = input("Please enter a single character: ").lower()
-	attempts -= 1
+	guess = raw_input("Please enter a single character: ")
 	for i, c in enumerate(target):
-		if c in target:
-			pass
+		if guess == c:
+			print True
+		else:
+			attempts -= 1
+			print False

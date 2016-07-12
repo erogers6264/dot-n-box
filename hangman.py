0b81f26ba6edb8db50bbd3260a0d06ccde6656fc
Ethan Rogers <erogers6264@gmail.com>
2016-07-12 11:30:48 -0700

    Refactor check guess code to its own function

--- hangman.py
+++ hangman.py
@@ -1,12 +1,12 @@
 from wordbank import randomWord
+import string
 
+attempts = input("How many attempts to guess would you like? ")
+attempts_remaining = attempts
+target = randomWord()
+blanks = ['*'] * len(target)
+incorrect_guesses = []
 
-def startGame():
-	attempts = input("How many attempts to guess would you like? ")
-	attempts_remaining = attempts
-	target = randomWord()
-	blanks = '*' * len(target)
-	incorrect_guesses = []
 
 
 def getGuess():
@@ -15,20 +15,20 @@ def getGuess():
 	guess = raw_input("Please enter a single character: ")
 	return guess
 
-
-def makeMove():
-	for x in xrange(1, attempts+1):
-		g = getGuess()
+def checkGuess(guess, target):
 		for i, char in enumerate(target):
 			if g == char:
 				print("OMG! That letter is in the word.")
 				blanks[i] = char
-				print blanks
+				print string.join(blanks, '')
 			else:
-				attempts -= 1
 				print("Not in word. You have {} attempts remaining".format(attempts))
 
 
-def function():
-	pass
+def makeMove(attempts):
+	while attempts > 0:
+		g = getGuess()
+		attempts -= 1
+
+makeMove(attempts)
 

46496e0210a44f8c719774ad146e2a7d621203c8
Ethan Rogers <erogers6264@gmail.com>
2016-07-13 12:25:57 -0700

    Correct field increment

--- models.py
+++ models.py
@@ -79,7 +79,7 @@ class GameForm(messages.Message):
 class NewGameForm(messages.Message):
     """Used to create a new game"""
     user_name = messages.StringField(1, required=True)
-    attempts = messages.IntegerField(4, default=5)
+    attempts = messages.IntegerField(2, default=5)
 
 
 class MakeMoveForm(messages.Message):

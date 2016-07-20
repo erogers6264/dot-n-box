4617593d97c687b5d115735de0421fd307409aa5
Ethan Rogers <erogers6264@gmail.com>
2016-07-19 21:11:31 -0700

    Add encoding to models

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-`
 """models.py - This file contains the class definitions for the Datastore
 entities used by the Game. Because these classes are also regular Python
 classes they can include methods (such as 'to_form' and 'new_game')."""
